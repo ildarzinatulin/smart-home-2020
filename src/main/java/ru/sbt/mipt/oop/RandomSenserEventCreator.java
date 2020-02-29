2397e14c5386234cf9f89a8aff5a577aea298731
ILdar Zinatulin <ildarzinatulin@MacBook-Air-ILdar.local>
2020-02-29 18:53:29 +0300

    version 1

---
+++ src/main/java/ru/sbt/mipt/oop/RandomSenserEventCreator.java
@@ -0,0 +1,19 @@
+package ru.sbt.mipt.oop;
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+class RandomSenserEventCreator {
+
+    static SensorEvent getNextSensorEvent() {
+        // pretend like we're getting the events from physical world, but here we're going to just generate some random events
+        if (Math.random() < 0.05) return null; // null means end of event stream
+        SensorEventType sensorEventType = SensorEventType.values()[(int) (4 * Math.random())];
+        String objectId = "" + ((int) (10 * Math.random()));
+        return new SensorEvent(sensorEventType, objectId);
+    }
+
+}
