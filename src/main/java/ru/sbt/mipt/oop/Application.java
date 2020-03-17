0b4b7b9daf7e97cd793e483a1ae916c3b37661c1
ILdar Zinatulin <ildarzinatulin@MacBook-Air-ILdar.local>
2020-03-17 16:34:37 +0300

    update

--- src/main/java/ru/sbt/mipt/oop/Application.java
+++ src/main/java/ru/sbt/mipt/oop/Application.java
@@ -1,14 +1,8 @@
 package ru.sbt.mipt.oop;
 
-import com.google.gson.Gson;
-
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
-
-import static ru.sbt.mipt.oop.SensorEventType.*;
 
 public class Application {
 
