7c1e3c58d5c3ec2527413a33b0aff577e4d19e7a
ILdar Zinatulin <ildarzinatulin@MacBook-Air-ILdar.local>
2020-03-26 11:34:56 +0300

    update

--- src/main/java/ru/sbt/mipt/oop/MyConfiguration.java
+++ src/main/java/ru/sbt/mipt/oop/MyConfiguration.java
@@ -2,10 +2,12 @@ package ru.sbt.mipt.oop;
 
 import com.coolcompany.smarthome.events.SensorEventsManager;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import java.util.Arrays;
 import java.util.List;
 
+@Configuration
 public class MyConfiguration {
 
     @Bean
